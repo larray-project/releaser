ab627823d4d79bed49ed7f86a742c196476a7de7
Gaëtan de Menten <gdementen@gmail.com>
2019-11-12 17:13:56 +0100

    changed release step funcs to take keyword arguments instead of a single config argument
    this requires **extra_kwargs everywhere (which is ugly) but makes the code more maintainable
    (as it is much more obvious which step func needs what) and makes it easier to call those functions standalone
    
    also added changelog_index_template argument for next_release functions

--- releaser/next_release.py
+++ releaser/next_release.py
@@ -5,14 +5,24 @@ from os.path import join
 from shutil import copy
 
 from releaser.utils import relname2fname, no, short, echocall, chdir
-from releaser.make_release import update_version, push
+from releaser.make_release import update_version
 
 
-def update_changelog(config):
-    if config['src_documentation'] is not None:
-        chdir(config['build_dir'])
-        release_name = config['release_name']
-        src_documentation = config['src_documentation']
+DEFAULT_CHANGELOG_INDEX_TEMPLATE = """{title}
+{underline}
+
+In development.
+
+.. include:: ./changes/{fname}
+
+
+"""
+
+
+def update_changelog(src_documentation, build_dir, release_name,
+                     changelog_index_template=DEFAULT_CHANGELOG_INDEX_TEMPLATE):
+    if src_documentation is not None:
+        chdir(build_dir)
 
         fname = relname2fname(release_name)
 
@@ -21,51 +31,32 @@ def update_changelog(config):
         changelog_file = join(changes_dir, fname)
         copy(join(changes_dir, 'template.rst.inc'), changelog_file)
 
-        # include release changelog in changes.rst
-        fpath = join(src_documentation, 'changes.rst')
-        changelog_index_template = """{title}
-{underline}
-
-In development.
-
-.. include:: {fpath}
-
-
-"""
-
-        with open(fpath) as f:
-            lines = f.readlines()
-            title = f"Version {short(release_name)}"
-            if lines[3] == title + '\n':
-                print(f"changes.rst not modified (it already contains {title})")
-                return
-            this_version = changelog_index_template.format(title=title,
-                                                           underline="=" * len(title),
-                                                           fpath='./changes/' + fname)
-            lines[3:3] = this_version.splitlines(True)
-        with open(fpath, 'w') as f:
-            f.writelines(lines)
-        with open(fpath, encoding='utf-8-sig') as f:
-            print('\n'.join(f.read().splitlines()[:20]))
-        if no('Does the full changelog look right?'):
-            exit(1)
-        echocall(['git', 'add', fpath, changelog_file])
+    # include release changelog in changes.rst
+    fpath = join(src_documentation, 'changes.rst')
+    with open(fpath) as f:
+        lines = f.readlines()
+        title = f"Version {short(release_name)}"
+        if lines[3] == title + '\n':
+            print(f"changes.rst not modified (it already contains {title})")
+            return
+        this_version = changelog_index_template.format(title=title, underline="=" * len(title), fname=fname)
+        lines[3:3] = this_version.splitlines(True)
+    with open(fpath, 'w') as f:
+        f.writelines(lines)
+    with open(fpath, encoding='utf-8-sig') as f:
+        print('\n'.join(f.read().splitlines()[:20]))
+    if no('Does the full changelog look right?'):
+        exit(1)
+    echocall(['git', 'add', fpath, changelog_file])
 
 
-def add_release(local_repository, package_name, module_name, release_name, branch='master', src_documentation=None):
-    config = {
-        'branch': branch,
-        'release_name': release_name,
-        'package_name': package_name,
-        'module_name': module_name,
-        'repository': local_repository,
-        'build_dir': local_repository,
-        'src_documentation': src_documentation,
-        'public_release': True
-    }
-    update_changelog(config)
-    config['release_name'] += '-dev'
-    update_version(config)
+def add_release(local_repository, package_name, module_name, release_name, branch='master', src_documentation=None,
+                changelog_index_template=DEFAULT_CHANGELOG_INDEX_TEMPLATE):
+    update_changelog(src_documentation, build_dir=local_repository, release_name=release_name,
+                     changelog_index_template=changelog_index_template)
+    release_name += '-dev'
+
+    update_version(build_dir=local_repository, release_name=release_name, package_name=package_name,
+                   module_name=module_name)
     # we should NOT push by default as next_release can be called when the working copy is on a branch (when we want to
     # add release notes for features which are not targeted for the current release)
-    # push(config)
